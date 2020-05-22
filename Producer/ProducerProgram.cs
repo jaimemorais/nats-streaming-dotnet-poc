d2df1238a8b1bb0bcf5b9630f7b70df9f9d95bb9
Jaime Morais <jaime.morais@serpro.gov.br>
2020-05-22 15:08:07 -0300

    Add producer

---
+++ Producer/ProducerProgram.cs
@@ -0,0 +1,49 @@
+﻿using STAN.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Produtor
+{
+    public static class ProducerProgram
+    {
+               
+        //  .\nats-streaming-server.exe -cid cluster-teste-local -store file -dir dados-cluster-teste-local
+
+        static void Main(string[] args)
+        {
+            string CLUSTER_ID = "cluster-teste-local";
+            string CLIENT_ID = "Producer-"+DateTime.Now.Second;
+            string SUBJECT = "subject-test";            
+            string URL_NATS = "nats://localhost:4222";
+
+            var cf = new StanConnectionFactory();
+            StanOptions stanOptions = StanOptions.GetDefaultOptions();
+            stanOptions.NatsURL = URL_NATS;
+
+            Console.WriteLine($"Connecting (URL_NATS = {URL_NATS}) ...");
+            Console.WriteLine($"CLUSTER_ID = {CLUSTER_ID}");
+            Console.WriteLine($"CLIENT_ID = {CLIENT_ID}");
+
+
+            using (var c = cf.CreateConnection(CLUSTER_ID, CLIENT_ID, stanOptions))
+            {
+
+                while (true)
+                {
+                    Random r = new Random();
+                    var msgId = r.Next();
+
+                    string msg = CLIENT_ID + "-" + msgId.ToString();
+                    c.Publish(SUBJECT, Encoding.UTF8.GetBytes(msg));
+
+                    Console.WriteLine($"Msg published on {SUBJECT} : {msg}");
+
+                    Thread.Sleep(1000);
+                }                
+                
+            }
+
+        }
+    }
+}
